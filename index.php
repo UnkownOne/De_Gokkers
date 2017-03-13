ac7181410d3f505fd62ed51530ace6d3631e1940
UnkownOne <D201664@edu.rocwb.nl>
2017-03-13 13:27:38 +0100

    project 3
    het hele project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>De Gokkers</title>
+</head>
+<body>
+<div class="header">
+
+</div>
+<div class="maincontent">
+	<div class="infobox">
+	</div>
+</div>	
+</body>
+</html>
